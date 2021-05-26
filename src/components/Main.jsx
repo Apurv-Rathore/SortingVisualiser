df428ddee1dc28913bc6f9bfb578b22a338ffcbf
apurvr-newzera <2019csb1077@iitrpr.ac.in>
2021-05-26 15:14:43 +0530

    base

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Navbar from "../components/Navbar";
+
+export default class Main extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return(
+            < Navbar />
+        );
+    }
+    
+
+
+}
+
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            ecgfd
+        </div>
+    );
+}
+
+export default Navbar;
