25c41d0a5ac3daa3f828e9571ac8696e9fcac3fe
apurvr-newzera <2019csb1077@iitrpr.ac.in>
2021-06-05 15:46:39 +0530

    everything done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,33 +5,71 @@ import bubbleSort from "../algorithms/bubbleSort";
 import insertionSort from "../algorithms/insertionSort";
 import selectionSort from "../algorithms/selectionSort";
 import quickSort from "../algorithms/quickSort";
-import mergeSort from "../algorithms/mergeSort"
+import mergeSort from "../algorithms/mergeSort";
 
 //algorithms
 const Navbar = ({ array, setArray, setColorChangeIndex }) => {
   const [sliderValue, setsliderValue] = useState(50);
   const handleChange = (evt) => {
     setsliderValue(evt.target.value);
-    setArray(generateArray(Math.floor((parseInt(evt.target.value) + 3) * 1.25)));
+    setArray(
+      generateArray(Math.floor((parseInt(evt.target.value) + 3) * 1.25))
+    );
   };
 
   const generateArray = (len) => {
-    
-    return  Array.from({length: len}, () => Math.floor(Math.random() * len));
+    return Array.from({ length: len }, () => Math.floor(Math.random() * len));
   };
 
   const generateNewArray = () => {
     setArray(generateArray(Math.floor((parseInt(sliderValue) + 3) * 1.25)));
     console.log(array);
-  }
+  };
 
   return (
     <div className="navbar">
-      <button onClick={()=> {mergeSort({array,setArray,setColorChangeIndex})}}>Merge Sort</button>
-      <button onClick={()=> {quickSort({array,setArray,setColorChangeIndex})}}>Quick Sort</button>
-      <button onClick={()=> {insertionSort({array,setArray,setColorChangeIndex})}}>Insertion Sort</button>
-      <button onClick={()=> {bubbleSort({array,setArray,setColorChangeIndex})}}>Bubble Sort</button>
-      <button onClick={()=> {selectionSort({array,setArray,setColorChangeIndex})}}>Selection Sort</button>
+      
+      
+      <button
+        className="glow-on-hover"
+        onClick={() => {
+          mergeSort({ array, setArray, setColorChangeIndex });
+        }}
+      >
+        Merge Sort
+      </button>
+      <button
+        className="glow-on-hover"
+        onClick={() => {
+          quickSort({ array, setArray, setColorChangeIndex });
+        }}
+      >
+        Quick Sort
+      </button>
+      <button
+        className="glow-on-hover"
+        onClick={() => {
+          insertionSort({ array, setArray, setColorChangeIndex });
+        }}
+      >
+        Insertion Sort
+      </button>
+      <button
+        className="glow-on-hover"
+        onClick={() => {
+          bubbleSort({ array, setArray, setColorChangeIndex });
+        }}
+      >
+        Bubble Sort
+      </button>
+      <button
+        className="glow-on-hover"
+        onClick={() => {
+          selectionSort({ array, setArray, setColorChangeIndex });
+        }}
+      >
+        Selection Sort
+      </button>
       <div className="slidecontainer">
         <input
           type="range"
@@ -43,13 +81,9 @@ const Navbar = ({ array, setArray, setColorChangeIndex }) => {
           onChange={handleChange}
         />
       </div>
-      <button onClick={generateNewArray}>Generate New Array </button>
+      <button className="glow-on-hover" onClick={generateNewArray}>Generate New Array </button>
     </div>
   );
 };
-
-
-
-
 
 export default Navbar;
