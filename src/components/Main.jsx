980e7bf17575bdd1f9dfe104e260cb1419cb4c67
apurvr-newzera <2019csb1077@iitrpr.ac.in>
2021-06-05 13:27:58 +0530

    color displayed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,37 +8,62 @@ import "./Main.css";
 //     super(props);
 //   }
 const Main = () => {
-  const [array, setArray] = useState([344,5234,534,4234,345,345,234,24,324,534,3444,35,423,24,23,123,13]);
-  
+  const getHeight = (number) => {
+    const height = Math.floor((number / Math.max.apply(Math, array)) * 500);
+    return height;
+  };
+
+  const [colorChangeIndex,setColorChangeIndex] = useState([]);
+
+  const [array, setArray] = useState([
+    344, 5234, 534, 4234, 345, 345, 234, 24, 324, 534, 3444, 35, 423, 24, 23,
+    123, 13,
+  ]);
+
+  const [divStyle, setDivStyle] = useState({
+    backgroundColor: "red",
+  });
+
+  const setColor = () => {
+
+  }
+
+
+
   const getWidth = () => {
     const width = Math.floor(1000 / array.length);
     return width;
   };
-  const getHeight = (number) => {
-    const height = Math.floor((number / Math.max.apply(Math, array)) * 500);
-    
-    // console.log(height, number, Math.max(array));
-    return height;
-  };
+
+
+  const isthis = (idx) =>{
+    if (idx===1 || idx===3){
+      return false;
+    }
+    // console.log("fff");
+    return true;
+  }
+
   return (
     <div>
       <Navbar array={array} setArray={setArray} />
       <div className="ArrayContainer" id="myDiv">
-
         {array.map((value, idx) => (
+          // height: `${getHeight(value)}px`,
+          // divStyle.height = `${getHeight(value)}px`;
           <div
             className="array-bar"
             key={idx}
             style={{
-              display:"inline-block",
-              backgroundColor: "red",
-              
+              display: "inline-block",
+              // backgroundColor: "red",
+              backgroundColor: `${ isthis(idx)?"blue":"red" }`,
               height: `${getHeight(value)}px`,
-              width: `${Math.floor((600)/(array.length))}px`,
-              marginLeft: `${Math.floor((600)/(array.length))*0.5}px`,
+              width: `${Math.floor(600 / array.length)}px`,
+              marginLeft: `${Math.floor(600 / array.length) * 0.5}px`
             }}
-          > 
-{/* etrfx */}
+          >
+            {/* etrfx */}
           </div>
         ))}
       </div>
