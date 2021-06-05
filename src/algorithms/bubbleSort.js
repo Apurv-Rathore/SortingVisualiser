04572bfeceea0805cfc50aef089bd28cd53f2722
apurvr-newzera <2019csb1077@iitrpr.ac.in>
2021-06-05 12:12:39 +0530

    bubble sort doing

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -1,4 +1,18 @@
 const bubbleSort = ({array, setArray}) => {
-    setArray([1,2,3]);
+    for (let index = 0; index < 2; index++) {
+        if (index===0){
+            setTimeout(() => {
+                setArray([4,3,2,1,1,1,1]);    
+            }, 1000*index);
+        }
+        if (index===1){
+            setTimeout(() => {
+                setArray([5,4,3,2,2,2,1]);    
+            }, 1000*index);
+        }
+    }
+    
+    
+
 }
 export default bubbleSort;
