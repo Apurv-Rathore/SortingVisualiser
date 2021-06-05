ac9477bcc9a7b67dc9865f4e9e93df31620971d8
dhakad-anant <2019csb1070@iitrpr.ac.in>
2021-06-05 15:26:30 +0530

    errorr removed quicksort

--- src/algorithms/quickSort.js
+++ src/algorithms/quickSort.js
@@ -48,8 +48,8 @@ const quickSort = ({ array, setArray, setColorChangeIndex }) => {
         let index = partition(arr, start, end);
 
         // Recursively apply the same logic to the left and right subarrays
-        quickSort(arr, start, index - 1);
-        quickSort(arr, index + 1, end);
+        quickSortRecursive(arr, start, index - 1);
+        quickSortRecursive(arr, index + 1, end);
     }
 
     quickSortRecursive(auxArray, 0, len-1)
@@ -57,44 +57,13 @@ const quickSort = ({ array, setArray, setColorChangeIndex }) => {
       setTimeout(() => {
         setArray(arrays[index]);
         setColorChangeIndex([...indexes[index]])
-      }, (index * 700)/(len));
+      }, (index * 3000)/(len));
     }
     setTimeout(() => {
       setColorChangeIndex([])
-    }, (arrays.length * 700)/(len));
+    }, (arrays.length * 3000)/(len));
     return arrays;
 };
 export default quickSort;
   
 
-
-    // function quickSortRecursive(arr, start, end) {
-    //     // Base case or terminating case
-    //     if (start >= end) {
-    //         return;
-    //     }
-
-    //     // Returns pivotIndex
-    //     let index = partition(arr, start, end);
-
-    //     // Recursively apply the same logic to the left and right subarrays
-    //     quickSort(arr, start, index - 1);
-    //     quickSort(arr, index + 1, end);
-    // }
-    // function partition(arr, start, end){
-    //     // Taking the last element as the pivot
-    //     const pivotValue = arr[end];
-    //     let pivotIndex = start; 
-    //     for (let i = start; i < end; i++) {
-    //         if (arr[i] < pivotValue) {
-    //         // Swapping elements
-    //         [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
-    //         // Moving to next element
-    //         pivotIndex++;
-    //         }
-    //     }
-        
-    //     // Putting the pivot value in the middle
-    //     [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]] 
-    //     return pivotIndex;
-    // };
